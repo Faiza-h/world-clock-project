9e9f77fcf456e4e11b84cd73e83ff419b9306ec6
Faiza-Hoosein <faiza.hoosein@gmail.com>
2025-08-01 19:21:23 +0200

    Added Javascipt coding

---
+++ javascript/index.js
@@ -0,0 +1,52 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#los-angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
+
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM	Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+
+  let chinaElement = document.querySelector("#china");
+  if (chinaElement) {
+    let chinaDateElement = chinaElement.querySelector(".date");
+    let chinaTimeElement = chinaElement.querySelector(".time");
+    let chinaTime = moment().tz("Asia/Shanghai");
+
+    chinaDateElement.innerHTML = chinaTime.format("MMMM	Do YYYY");
+    chinaTimeElement.innerHTML = chinaTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
